8ab83dacec8f79fa593b9dedc832eeeee6dea868
Ishy720 <76483561+Ishy720@users.noreply.github.com>
2023-10-15 22:31:59 +0100

    Removed old method
    Method no longer used

--- rest-api/src/main/kotlin/com/example/api/restapi/repositories/PersonRepository.kt
+++ rest-api/src/main/kotlin/com/example/api/restapi/repositories/PersonRepository.kt
@@ -7,13 +7,4 @@ import org.springframework.stereotype.Repository
 import org.springframework.data.jpa.repository.JpaRepository
 
 @Repository
-interface PersonRepository : JpaRepository<Person, Long> {
-
-    fun findByUsername(username: String): Person?
-
-}
-
-/*
-//Create a Person Repository to interact with the database
-@Repository
-interface PersonRepository : JpaRepository<Person, Long>*/
+interface PersonRepository : JpaRepository<Person, Long>
